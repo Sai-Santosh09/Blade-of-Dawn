5878632aeab36e87016a4ae151ae941443e79c80
Sai-Santosh09 <saisantosh2712@gmail.com>
2026-07-03 21:42:26 +0530

    added the new transiton type and made it fucntional

--- Levels/scripts/level_transition_interact.gd
+++ Levels/scripts/level_transition_interact.gd
@@ -9,15 +9,21 @@ func _ready() -> void:
 
 
 func player_interact() -> void:
-	
+	_player_entered( PlayerManager.player )
 	pass
 
 
 func _on_area_entered( _a : Area2D ) -> void:
-	
+	PlayerManager.interact_pressed.connect( player_interact )
 	pass
 
 
 func _on_area_exited( _a : Area2D ) -> void:
-	
+	PlayerManager.interact_pressed.disconnect( player_interact )
+	pass
+
+
+func _update_area() -> void:
+	super()
+	collision_shape.shape.size = Vector2( 32, 32 )
 	pass
