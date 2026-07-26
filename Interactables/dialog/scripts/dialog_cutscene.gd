ac27a6d44dd07f9d9c5032018c7c8870cd435e9f
Sai-Santosh09 <saisantosh2712@gmail.com>
2026-07-26 18:15:37 +0530

    completed the basic implementation of Dialog cutscene

--- Interactables/dialog/scripts/dialog_cutscene.gd
+++ Interactables/dialog/scripts/dialog_cutscene.gd
@@ -8,7 +8,7 @@ enum Mode { PARRALLEL, SEQUENTIAL }
 @export var playback_mode : Mode = Mode.SEQUENTIAL
 
 var actions : Array[ CutsceneAction ] = []
-var action_finished_count : int = 0
+var actions_finished_count : int = 0
 
 
 func _ready() -> void:
@@ -20,4 +20,30 @@ func gather_actions() -> void:
 	for c in get_children():
 		if c is CutsceneAction:
 			actions.append( c )
+			if Engine.is_editor_hint() == false:
+				c.finished.connect( _on_action_finished )
+	pass
+
+
+func play() -> void:
+	if Engine.is_editor_hint():
+		return
+	actions_finished_count = 0
+	if actions.size() == 0:
+		await get_tree().process_frame
+		finished.emit()
+	elif playback_mode == Mode.SEQUENTIAL:
+		actions[ 0 ].play()
+	else:
+		for a in actions:
+			a.play()
+	pass
+
+
+func _on_action_finished() -> void:
+	actions_finished_count += 1
+	if actions_finished_count >= actions.size():
+		finished.emit()
+	elif playback_mode == Mode.SEQUENTIAL:
+		actions[ actions_finished_count ].play()
 	pass
